cac97a77f8d551a971b366834d63a90efd176d4e
Juan David Sanchez <juan.david748@gmail.com>
2025-04-06 20:35:30 -0500

    Add files via upload

---
+++ Maestria/src/main/java/co/edu/uniquindio/maestria/MaestriaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.maestria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaestriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaestriaApplication.class, args);
+    }
+
+}
---
+++ Maestria/src/test/java/co/edu/uniquindio/maestria/MaestriaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.maestria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaestriaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
